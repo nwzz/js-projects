e4c8741fb228b04b0a5e6769a6b5a9a137bbeca7
Rabeya Begum <shahnewaz@mail.com>
2021-02-04 19:47:19 +0600

    splite

---
+++ splite.js
@@ -0,0 +1,8 @@
+ const num = [1,2,3,4,5,6,7,8];
+ const part = num.slice(2,5);
+ console.log(part);
+ const parts = num.splice(2,3, 45);
+ console.log(parts);
+ console.log(num);
+const togather = num.join(" k ");
+console.log(togather);
