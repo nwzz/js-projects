010d7e00f7b6cf150b4952e0cef76eb23e9d1489
Rabeya Begum <shahnewaz@mail.com>
2021-02-04 13:35:15 +0600

    new

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// falsy
+// 0
+// NaN
+// ""
+// undefined
+//truthy
+//[]
+//" "
+
+
+let name = [];
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
